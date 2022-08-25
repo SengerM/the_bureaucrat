4b136561a41126844f0c91d555d46aecd5cd279e
Matias Senger <m.senger@hotmail.com>
2022-08-25 12:21:31 +0200

    Added docstrings

--- the_bureaucrat/bureaucrats.py
+++ the_bureaucrat/bureaucrats.py
@@ -7,6 +7,9 @@ import tempfile
 import traceback
 
 def find_ugly_characters_better_to_avoid_in_paths(path:Path):
+	"""Returns a set of characters that are considered as not nice options
+	within a Path (e.g. a white space, better to avoid), if any of such
+	characters are found. This function is based on [this](https://stackoverflow.com/a/1976172/8849755)."""
 	if not isinstance(path, (Path,str)):
 		raise ValueError(f'`path` must be an instance of Path.')
 	NICE_CHARACTERS_FOR_FILE_AND_DIRECTORY_NAMES = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '_', '-'} # https://stackoverflow.com/a/1976172/8849755
@@ -25,48 +28,121 @@ def create_run(path_where_to_create_the_run:Path, run_name:str)->Path:
 	return path_to_run
 
 def exists_run(path_where_to_find_the_run:Path, run_name:str)->bool:
+	"""Returns `True` or `False` depending on whether the `run_name` is
+	present in `path_where_to_find_the_run`."""
 	path_to_run = path_where_to_find_the_run/run_name
 	return (path_to_run/'bureaucrat_run_info.txt').is_file()
 
 class RunBureaucrat:
 	def __init__(self, path_to_the_run:Path):
+		"""Create a `RunBureaucrat`.
+		
+		Arguments
+		---------
+		path_to_the_run: Path
+			A path to the run. The last element of the path will be the
+			run name.
+		"""
 		if not isinstance(path_to_the_run, Path):
 			raise TypeError(f'`path_to_the_run` must be an instance of {Path}, received object of type {type(path_to_the_run)}. ')
 		self._path_to_the_run = path_to_the_run
 	
 	@property
 	def path_to_run_directory(self)->Path:
+		"""Return a `Path` object pointing to the run's directory."""
 		return self._path_to_the_run
 	
 	@property
 	def run_name(self)->str:
+		"""Returns a string with the name of the run."""
 		return self._path_to_the_run.parts[-1]
 	
 	@property
 	def path_to_temporary_directory(self)->Path:
-		"""Path to a temporary directory to store temporary stuff. It will
-		be lost after Python ends."""
+		"""Returns a `Path` pointing to a temporary directory to store stuff. 
+		It will	be lost after Python ends."""
 		if not hasattr(self, '_temporary_directory'):
 			self._temporary_directory = tempfile.TemporaryDirectory()
 		return Path(self._temporary_directory.name)
 		
 	def path_to_directory_of_task(self, task_name:str)->Path:
+		"""Returns a `Path` pointing to the directory of a task named 
+		`task_name` within the run being handled by this `RunBureaucrat`.
+		Note that such directory needs not to exist. 
+		
+		Arguments
+		---------
+		task_name: str
+			The name of the task for which you want the path to its directory.
+		"""
 		return self.path_to_run_directory/task_name
 	
-	def list_subruns_of_task(self, task_name:str)->Path:
-		return {p.parts[-1]: p for p in (self.path_to_directory_of_task(task_name)/'subruns').iterdir()}
+	def list_subruns_of_task(self, task_name:str)->dict:
+		"""Returns a dictionary of the form
+		```
+		{
+			run_name: path,
+			run_name: path,
+			...
+		}
+		```
+		where `run_name` are the names of the subruns for the `task_name` 
+		and `path` are `Path` objects pointing to each subrun. If the task
+		has no subruns, or if it does not exist, `None` is returned.
+		
+		Arguments
+		---------
+		task_name: str
+			The name of the task.
+		"""
+		try:
+			return {p.parts[-1]: p for p in (self.path_to_directory_of_task(task_name)/'subruns').iterdir()}
+		except FileNotFoundError:
+			return None
 	
 	def was_task_run_successfully(self, task_name:str)->bool:
-		was_it = False
+		"""If `task_name` was successfully run beforehand returns `True`,
+		otherwise (task does not exist or it does but was not completed)
+		returns `False`.
+		
+		Arguments
+		---------
+		task_name: str
+			The name of the task.
+		
+		Returns
+		-------
+		was_run: bool
+			`True` or `False` telling if the tasks was run successfully
+			or not.
+		"""
+		was_run = False
 		try:
 			with open(self.path_to_directory_of_task(task_name)/'bureaucrat_task_report.txt', 'r') as ifile:
 				if 'exit_status: task completed successfully with no errors :)' in ifile.readline():
-					was_it = True
+					was_run = True
 		except Exception:
 			pass
-		return was_it
+		return was_run
 	
 	def check_these_tasks_were_run_successfully(self, tasks_names:list, raise_error:bool=True)->bool:
+		"""Check that certain tasks were run successfully beforehand.
+		
+		Arguments
+		---------
+		tasks_names: list of str, or str
+			A list with the names of the tasks to check for, or a string
+			with the name of a single task to check for.
+		raise_error: bool, default True
+			If `True` then a `RuntimeError` will be raised if any of the
+			tasks in `tasks_names` was not run successfully beforehand.
+			If `False` then no error is raised.
+		
+		Returns
+		-------
+		were_run: bool
+			`True` or `False` telling if all the tasks were run.
+		"""
 		if isinstance(tasks_names, str):
 			tasks_names = [tasks_names]
 		if not isinstance(tasks_names, list) or not all([isinstance(task_name, str) for task_name in tasks_names]):
@@ -76,23 +152,69 @@ class RunBureaucrat:
 			raise RuntimeError(f'Not all tasks {tasks_names} were successfully run beforehand. ')
 		return were_run
 	
-	def create_run(self):
-		if not exists_run(self.path_to_run_directory.parent, self.run_name):
+	def create_run(self, raise_error:bool=False):
+		"""Creates a run where this `RunBureaucrat` is pointing to.
+		
+		Arguments
+		---------
+		raise_error: bool, default False
+			If `True` a `RuntimeError` is raised if the run already exists. If
+			`False` no error is raised, if the run already exists in this
+			case nothing is done.
+		"""
+		if exists_run(self.path_to_run_directory.parent, self.run_name):
+			if raise_error:
+				raise RuntimeError(f'Cannot create run {self.run_name} in {self.path_to_run_directory} because it already exists.')
+		else:
 			create_run(
 				path_where_to_create_the_run = self.path_to_run_directory.parent,
 				run_name = self.run_name,
 			)
 	
-	def handle_task(self, task_name):
+	def handle_task(self, task_name:str):
+		"""This method is used to create a new "subordinate bureaucrat" 
+		of type `TaskBureaucrat` that will handle a task (instead of a
+		run) within the run being handled by the current `RunBureaucrat`.
+		
+		This method was designed for being used together with a `with` 
+		statement, e.g.
+		```
+		with a_run_bureaucrat.handle_task('some_task') as subordinated_task_bureaucrat:
+			blah blah blah
+		```
+		
+		Arguments
+		---------
+		task_name: str
+			The name of the task to handle by the new bureaucrat.
+		
+		Returns
+		-------
+		new_bureaucrat: TaskBureaucrat
+			A bureaucrat to handle the task.
+		"""
 		if len(find_ugly_characters_better_to_avoid_in_paths(task_name)) != 0:
 			warnings.warn(f'Your `task_name` is {repr(task_name)} and contains the character/s {find_ugly_characters_better_to_avoid_in_paths(task_name)} which is better to avoid, as this is going to be a path in the file system.')
-		return _TaskBureaucrat(
+		return TaskBureaucrat(
 			path_to_the_run = self.path_to_run_directory,
 			task_name = task_name,
 		)
 		
-class _TaskBureaucrat(RunBureaucrat):
+class TaskBureaucrat(RunBureaucrat):
 	def __init__(self, path_to_the_run:Path, task_name:str, drop_old_data:bool=True):
+		"""Create a `TaskBureaucrat`.
+		
+		Arguments
+		---------
+		path_to_the_run: Path
+			A `Path` object pointing to the run wherein to handle the task.
+		task_name: str
+			The name of this task.
+		drop_old_data: bool, default True
+			If `True`, the directory for this task will be cleaned when
+			this bureaucrat begins operating. Otherwise, any previous data
+			will be untouched.
+		"""
 		if not exists_run(path_to_the_run.parent, path_to_the_run.parts[-1]):
 			raise ValueError(f'`path_to_the_run` is {path_to_the_run} which does not look like the directory of a run...')
 		super().__init__(path_to_the_run=path_to_the_run)
@@ -100,11 +222,14 @@ class _TaskBureaucrat(RunBureaucrat):
 		self._drop_old_data = drop_old_data
 	
 	@property
-	def task_name(self):
+	def task_name(self)->str:
+		"""Returns a string with the name of the task."""
 		return self._task_name
 	
 	@property
-	def path_to_directory_of_my_task(self):
+	def path_to_directory_of_my_task(self)->Path:
+		"""Returns a `Path` object pointing to the directory of the current
+		task."""
 		return self.path_to_directory_of_task(self.task_name)
 	
 	def __enter__(self):
@@ -132,6 +257,18 @@ class _TaskBureaucrat(RunBureaucrat):
 				print(f'{exc_type.__name__}: {exc_value}', file=ofile)
 	
 	def create_subrun(self, subrun_name:str)->RunBureaucrat:
+		"""Create a subrun within the current task.
+		
+		Arguments
+		---------
+		subrun_name: str
+			The name for the subrun.
+		
+		Returns
+		-------
+		new_run_bureaucrat: RunBureaucrat
+			A newly created `RunBureaucrat` ready to handle the new subrun.
+		"""
 		some_bureaucrat = RunBureaucrat(path_to_the_run=self.path_to_directory_of_my_task/f'subruns/{subrun_name}')
 		some_bureaucrat.create_run()
 		return some_bureaucrat
